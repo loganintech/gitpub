c0be8fc052d749846e18a33413d302b345c66d35
Logan Saso <logan.saso2016@gmail.com>
2019-04-22 22:04:01 -0700

    Skip serializing None fields

--- src/provider/github.rs
+++ src/provider/github.rs
@@ -20,85 +20,97 @@ pub struct GithubArgs {
         long = "description",
         help = "A short description of the repository."
     )]
+    #[serde(skip_serializing_if = "Option::is_none")]
     description: Option<String>,
     #[structopt(
         long = "homepage",
         help = "A URL with more information about the repository."
     )]
+    #[serde(skip_serializing_if = "Option::is_none")]
     homepage: Option<String>,
     #[structopt(
         short = "p",
         long = "private",
         help = "Sets the repository to private. Requires 'repo' scope on your personal access token"
     )]
+    #[serde(skip_serializing_if = "Option::is_none")]
     private: Option<bool>,
     #[structopt(
         short = "i",
         long = "issues",
         help = "Enable or disable issues for this repo. Defaults to true."
     )]
+    #[serde(skip_serializing_if = "Option::is_none")]
     has_issues: Option<bool>,
     #[structopt(
         long = "projects",
         help = "Enables or disables projects for this repo. Defaults to true."
     )]
+    #[serde(skip_serializing_if = "Option::is_none")]
     has_projects: Option<bool>,
     #[structopt(
         short = "w",
         long = "wiki",
         help = "Enables or disables wikis for this repo. Defaults to true."
     )]
+    #[serde(skip_serializing_if = "Option::is_none")]
     has_wiki: Option<bool>,
     #[structopt(
         long = "team",
         help = "Id of the team that has access to this repo. Only valid when using --org.",
         requires = "org"
     )]
+    #[serde(skip_serializing_if = "Option::is_none")]
     team_id: Option<u32>,
     #[structopt(
         short = "r",
         long = "initialize_with_readme",
         help = "Creates an initial commit with empty README.md. Defaults to false."
     )]
+    #[serde(skip_serializing_if = "Option::is_none")]
     auto_init: Option<bool>,
     #[structopt(
         long = "gitignore",
         help = "Language template: ex 'Rust'. View more templates at https://github.com/github/gitignore"
     )]
+    #[serde(skip_serializing_if = "Option::is_none")]
     gitignore_template: Option<String>,
     #[structopt(
         long = "license",
         help = "License template: ex 'mit' or 'mpl-2.0'. View more at https://choosealicense.com/"
     )]
+    #[serde(skip_serializing_if = "Option::is_none")]
     license_template: Option<String>,
     #[structopt(
         long = "squash",
         help = "Enables or disables squash-merging for this repo. Defaults to true."
     )]
+    #[serde(skip_serializing_if = "Option::is_none")]
     allow_squash_merge: Option<bool>,
     #[structopt(
         long = "merge",
         help = "Enables or disables merging with a merge commit. Defaults to true."
     )]
+    #[serde(skip_serializing_if = "Option::is_none")]
     allow_merge_commit: Option<bool>,
     #[structopt(
         long = "rebase",
         help = "Enables or disables rebase-merging for this repo. Defaults to true."
     )]
+    #[serde(skip_serializing_if = "Option::is_none")]
     allow_rebase_merge: Option<bool>,
-    #[serde(skip_serializing)]
     #[structopt(
         long = "org",
         help = "Creates the repo under an organization. Requires you have CREATE REPO permissions in that org."
     )]
     org: Option<String>,
-    #[serde(skip_serializing)]
     #[structopt(
         short = "e",
         long = "endpoint",
         help = "Allows redirection of requests to enterprise providers.",
         conflicts_with = "org"
     )]
+    #[serde(skip_serializing)]
     custom_endpoint: Option<String>,
 }
 
