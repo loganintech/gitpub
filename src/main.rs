712c7daa297ed8e3f6fd6b1e926df1d44c6f1255
Logan Saso <logansaso+tech@gmail.com>
2019-05-19 23:10:53 -0700

    Extracted some related logic

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,18 @@ mod cli;
 mod git;
 mod provider;
 
-use git::add_remote;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let matches = cli::get_app().get_matches();
     let config = Gitpo::from_matches(&matches);
     let config = config.as_provider();
     let client = reqwest::Client::new();
+
     let endpoint = if let Some(e) = matches.value_of("endpoint") {
         e.to_string()
     } else {
         config.endpoint()
     };
+
     let request = client
         .post(&endpoint)
         .body(config.payload())
@@ -28,41 +28,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let result = request.send()?;
     let status = result.status();
     let headers = result.headers();
+
     match status {
         StatusCode::OK | StatusCode::CREATED => {
             let apiloc = config.extract_url(&headers);
-            let (remote_url, can_use_ssh) = match (
-                config.ssh_url(&headers),
-                matches.is_present("ssh_remote_format"),
-            ) {
-                (Some(url), true) => (url, true),
-                _ => (config.extract_url(&headers), false),
-            };
             println!("Repo created: {}", apiloc);
-            let remote_name = matches
-                .value_of("remote_name")
-                .expect("This should default to origin, so something is wrong.");
-
-            if matches.is_present("set_remote") {
-                if matches.is_present("ssh_remote_format") && !can_use_ssh {
-                    eprintln!("Can't use ssh format with this provider.");
-                    exit(22);
-                }
-
-                if !add_remote(remote_name, &remote_url) {
-                    eprintln!("Failed to add remote.");
-                    exit(404);
-                }
-            }
-        },
+            set_remote(&matches, &headers, config);
+        }
         StatusCode::UNPROCESSABLE_ENTITY | StatusCode::BAD_REQUEST => {
             eprintln!("The provider had an issue processing this request. Perhaps the repository already exists, or you're using an unsupported option. e.g. Enabling projects on a repo in an org that has them disabled.");
             exit(2);
-        },
+        }
         StatusCode::UNAUTHORIZED => {
             eprintln!("You are unauthorized to create that repo.");
             exit(3);
-        },
+        }
         _ => {
             eprintln!("An unknown response was sent by the provider.");
             exit(42);
@@ -70,4 +50,30 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
 
     Ok(())
+}
+
+fn set_remote(matches: &clap::ArgMatches, headers: &reqwest::header::HeaderMap, config: &dyn provider::Provider) {
+    let (remote_url, can_use_ssh) = match (
+        config.ssh_url(&headers),
+        matches.is_present("ssh_remote_format"),
+    ) {
+        (Some(url), true) => (url, true),
+        _ => (config.extract_url(&headers), false),
+    };
+
+    let remote_name = matches
+        .value_of("remote_name")
+        .expect("This should default to origin, so something is wrong.");
+
+    if matches.is_present("set_remote") {
+        if matches.is_present("ssh_remote_format") && !can_use_ssh {
+            eprintln!("Can't use ssh format with this provider.");
+            exit(22);
+        }
+
+        if !git::add_remote(remote_name, &remote_url) {
+            eprintln!("Failed to add remote.");
+            exit(404);
+        }
+    }
 }
